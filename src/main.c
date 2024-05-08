639fd10b4cd5f2fd3afbb6a43c72d9df8b714cce
Georg-code <jornig@outlook.com>
2024-05-08 02:42:24 +0200

    some things work, some things need fixes

--- src/main.c
+++ src/main.c
@@ -19,15 +19,27 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 
+
 /* STEP 7 - Change the sleep time from 1000 ms to 100 ms */
 #define SLEEP_TIME_MS   100
 
 /* STEP 3.1 - Get the node identifier for button 1 through its alias sw0 */
-#define SW0_NODE	DT_ALIAS(albtn)
+
+#define ZEPHYR_USER_NODE DT_PATH(zephyr_user);
+
+#define ZEPHYR_USER_NODE DT_PATH(zephyr_user)
+
+const struct gpio_dt_spec signal =
+        GPIO_DT_SPEC_GET(ZEPHYR_USER_NODE, motor_gpios);
+
+/* Configure the pin */
+
 
 /* STEP 3.2 - Get the device pointer. pin number, and pin's configuration flags through gpio_dt_spec */
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+/*#define SW0_NODE DT_ALIAS(albtn);
 
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+*/
 /* LED0_NODE is the devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(stled)
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
@@ -82,7 +94,7 @@ static void simulate_data(void)
 static void app_led_cb(bool led_state)
 {
 
-	gpio_pin_set_dt(&led,led_state);
+	
 }
 
 
@@ -135,6 +147,26 @@ struct bt_conn_cb connection_callbacks = {
 
 int main(void)
 {
+
+int ret;
+
+	if (!device_is_ready(led.port)) {
+		return -1;
+	}
+	/* STEP 4 - Verify that the device is ready for use */
+
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return -1;
+	}
+
+	gpio_pin_configure_dt(&signal, GPIO_OUTPUT_ACTIVE);
+
+/* Set the pin to its active level */
+//	gpio_pin_set_dt(&signal, 1);
+
+
 	int err;
 
 	LOG_INF("Starting Lesson 4 - Exercise 2 \n");
@@ -169,36 +201,6 @@ int main(void)
 
 	LOG_INF("Advertising successfully started. You are ready to be safed\n");
 
-	
-int ret;
-
-	if (!device_is_ready(led.port)) {
-		return -1;
-	}
-	/* STEP 4 - Verify that the device is ready for use */
-	if (!device_is_ready(button.port)) {
-		return -1;
-	}
-
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return -1;
-	}
-
-	/* STEP 5 - Configure the pin connected to the button to be an input pin and set its hardware specifications */
-	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-	if (ret < 0) {
-		return -1;
-	}
-	while (1) {
-		/* STEP 6.1 - Read the status of the button and store it */
-        bool val = gpio_pin_get_dt(&button);
-
-		/* STEP 6.2 - Update the LED to the status of the button */
-        gpio_pin_set_dt(&led,val);
-
-        k_msleep(SLEEP_TIME_MS); // Put the main thread to sleep for 100ms for power optimization
-	}
 
 
 }
